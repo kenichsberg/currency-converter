650cbcc0c77cfbe9641241ab3d73b12cbe23b7f5
kenichsberg <kn1017356@gmail.com>
2025-04-28 00:59:24 +0300

    feat: add convert endpoint

--- currency-converter-api/src/routes.ts
+++ currency-converter-api/src/routes.ts
@@ -1,8 +1,41 @@
 import { Router, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+const envFile = '.env'
+dotenv.config({ path: envFile })
 
 const router = Router()
 
-router.get('', async (req, res, next) => {
+const SWOP_BASE_URL = 'https://swop.cx/rest'
+const swopHeaders = new Headers()
+swopHeaders.append('Authorization', `ApiKey ${process.env.SWOP_API_KEY}`)
+
+const getJSON = <T>(config: {
+  url: string
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+  headers: Headers
+}): Promise<T> => {
+  const fetchConfig = { method: 'GET', headers: config.headers }
+  return fetch(config.url, fetchConfig).then<T>((response) => response.json())
+}
+
+router.get('/convert/:from/:to/:amount', async (req, res, next) => {
+  const amount = parseFloat(req.params.amount)
+  if (isNaN(amount)) return res.send(400)
+
+  let swopRes = await getJSON<{ quote: number }>({
+    url: `${SWOP_BASE_URL}/rates/${req.params.from}/${req.params.to}`,
+    method: 'GET',
+    headers: swopHeaders,
+  })
+  if (swopRes === null) return res.send(400)
+
+  console.log('swopRes: ', swopRes)
+  const rate = swopRes.quote
+  console.log(typeof rate)
+  if (typeof rate !== 'number') return res.send(400)
+
+  return res.json({ converted: amount * rate })
 })
 
 export { router }
--- currency-converter-api/tests/integration.test.ts
+++ currency-converter-api/tests/integration.test.ts
@@ -1,3 +1,13 @@
-import { describe, it, expect, beforeEach, beforeAll } from "@jest/globals";
-import request from "supertest";
-import { app } from "../src/app";
+import { describe, it, expect, beforeEach, beforeAll } from '@jest/globals'
+import request from 'supertest'
+import { app } from '../src/app'
+
+it('', async () => {
+  const res = await request(app).get('/api/convert/EUR/USD/2.5')
+
+  expect(res.status).toBe(200)
+  expect(res.body).toHaveProperty('converted')
+  expect(typeof res.body.converted).toBe('number')
+  console.log(res.status)
+  console.log(res.body)
+})
